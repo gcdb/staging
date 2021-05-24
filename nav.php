1692eaa92b8b4a9ade6498fad680745566e3dadd
gcdb <54796686+gcdb@users.noreply.github.com>
2021-05-24 19:23:35 +0800

    php include

---
+++ nav.php
@@ -0,0 +1,46 @@
+		<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+			<div class="container">
+				<a class="navbar-brand" href="index">GCDB</a>
+				<button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-link" href="index">Home</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle active" href="#" data-toggle="dropdown">
+								Characters
+							</a>
+							<div class="dropdown-menu dropdown-menu-right greytheme" aria-labelledby="navbarDropdownPortfolio">
+								<a class="dropdown-item whitetext" href="characters">List</a>
+								<a class="dropdown-item whitetext" href="tierlist">Tierlist</a>
+								
+								<a class="dropdown-item whitetext" href="skillfilter">Skill Filter</a>
+								<a class="dropdown-item whitetext" href="unique">List of Uniques</a>
+								<a class="dropdown-item whitetext" href="association">Associations</a>
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="gamenews">Game News</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
+								Useful Information
+							</a>
+							<div class="dropdown-menu dropdown-menu-right greytheme" aria-labelledby="navbarDropdownPortfolio">
+								<a class="dropdown-item whitetext" href="cooking">Cooking</a>
+								<a class="dropdown-item whitetext" href="gears">Gears</a>
+								<a class="dropdown-item whitetext" href="statuseffect">Status Effects</a>
+								<a class="dropdown-item whitetext" href="boss">Bosses</a>
+								
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" style="color:#c2c4ff" href="https://sdsgc.gg">Global &#10132;</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
